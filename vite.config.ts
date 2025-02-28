39cbc79cac8db556e11ef84939cb8934a6b60ff2
QuertyIsABadPassword <39013081+QuertyIsABadPassword@users.noreply.github.com>
2025-02-27 22:58:18 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
+  base: '/repository-name/',
   plugins: [vue()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
