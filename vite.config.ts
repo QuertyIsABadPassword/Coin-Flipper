61b79d4ce28c618e7dad64ca03258a83670efd07
QuertyIsABadPassword <39013081+QuertyIsABadPassword@users.noreply.github.com>
2025-02-27 22:58:38 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  base: '/repository-name/',
+  base: '/Coin-Flipper/',
   plugins: [vue()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
